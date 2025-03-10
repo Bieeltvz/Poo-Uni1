d94cd5588a0cf47092b9e1f02961d4d7c4cea46f
Gabriel Tormena <110978689+Bieeltvz@users.noreply.github.com>
2025-03-10 20:46:20 -0300

    Update Funcionarios.java

--- atividade2/Funcionarios.java
+++ atividade2/Funcionarios.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package atividade2;
+package atividade3;
 
 /**
  *
- * @author Usuário
+ * @author gtvargas
  */
 public class Funcionarios {
 
@@ -18,6 +18,9 @@ public class Funcionarios {
     }
 
     public void setNome(String nome) {
+        if (nome.isEmpty() || nome.isBlank()){
+            throw new IllegalArgumentException("O nome eh invalido");
+        }
         this.nome = nome;
     }
 
@@ -26,6 +29,10 @@ public class Funcionarios {
     }
 
     public void setSalario(double salario) {
+        if (salario <= 0){
+            throw new IllegalArgumentException("O saldo eh invalido");
+        }
+        
         this.salario = salario;
     }
 
@@ -46,3 +53,17 @@ public class Funcionarios {
         return pagar;
     }
 }
+/**
+ *   private static final double FAIXA_1_LIMITE_SUP = 1903.98;
+    private static final double FAIXA_2_LIMITE_INF = 1903.99;
+    private static final double FAIXA_2_LIMITE_SUP = 2826.65;
+    private static final double FAIXA_3_LIMITE_INF = 2826.66;
+    private static final double FAIXA_3_LIMITE_SUP = 3751.05;
+    private static final double FAIXA_4_LIMITE_INF = 3751.06;
+    private static final double FAIXA_4_LIMITE_SUP = 4664.68;
+    private static final double FAIXA_5_LIMITE_INF = 4664.69;
+    private static final double ALIQUOTA_FAIXA_2 = 0.075;
+    private static final double ALIQUOTA_FAIXA_3 = 0.15;
+    private static final double ALIQUOTA_FAIXA_4 = 0.225;
+    private static final double ALIQUOTA_FAIXA_5 = 0.275;
+ */
