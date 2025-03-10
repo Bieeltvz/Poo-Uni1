e936ab6b4f53a9137c0b32274512d4b4bb695f69
Gabriel Tormena <110978689+Bieeltvz@users.noreply.github.com>
2025-03-10 20:45:50 -0300

    Update Funcionario.java

--- lista3/Funcionario.java
+++ lista3/Funcionario.java
@@ -19,6 +19,9 @@ public class Funcionario {
     }
 
     public void setNome(String nome) {
+        if (nome.isEmpty() || nome.isBlank()){
+            throw new IllegalArgumentException("O nome eh invalido");
+        }
         this.nome = nome;
     }
 
@@ -27,6 +30,9 @@ public class Funcionario {
     }
 
     public void setSalario(double salario) {
+        if (salario <= 0){
+            throw new IllegalArgumentException("O saldo eh invalido");
+        }
         
         this.salario = salario;
     }
@@ -34,17 +40,30 @@ public class Funcionario {
     public double cacularirpf() {
         double pagar = 0;
         if (salario <= 1903.98) {
-        return 0;
-    } else if (salario <= 2826.65) {
-        pagar = (salario * 0.075) - 142.80;
-    } else if (salario <= 3751.05) {
-        pagar = (salario * 0.15) - 354.80;
-    } else if (salario <= 4664.68) {
-        pagar = (salario * 0.225) - 636.13;
-    } else {
-        pagar = (salario * 0.275) - 869.36;
+            System.out.println("Isento de imposto de renda");
+        } else if (salario > 1903.98 && salario < 2826.65) {
+            pagar = (salario * (7.5 / 100));
+        } else if (salario > 2826.65 && salario < 3751.05) {
+            pagar = (salario * (15 / 100));
+        } else if (salario > 3751.05 && salario < 4664.68) {
+            pagar = (salario * (22.5 / 100));
+        } else {
+            pagar = (salario * (27.5 / 100));
+        }
+        return pagar;
     }
-
-    return pagar;
 }
-}
+/**
+ *   private static final double FAIXA_1_LIMITE_SUP = 1903.98;
+    private static final double FAIXA_2_LIMITE_INF = 1903.99;
+    private static final double FAIXA_2_LIMITE_SUP = 2826.65;
+    private static final double FAIXA_3_LIMITE_INF = 2826.66;
+    private static final double FAIXA_3_LIMITE_SUP = 3751.05;
+    private static final double FAIXA_4_LIMITE_INF = 3751.06;
+    private static final double FAIXA_4_LIMITE_SUP = 4664.68;
+    private static final double FAIXA_5_LIMITE_INF = 4664.69;
+    private static final double ALIQUOTA_FAIXA_2 = 0.075;
+    private static final double ALIQUOTA_FAIXA_3 = 0.15;
+    private static final double ALIQUOTA_FAIXA_4 = 0.225;
+    private static final double ALIQUOTA_FAIXA_5 = 0.275;
+ */
